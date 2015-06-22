d4acb9a0193b71346af207b87de80633fec8b59a
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2015-06-22 21:27:05 +0530

    Basic add/remove section UI

---
+++ manage_sections.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>R.D. Tea | Manage Sections</title>
+    </head>
+    <body>
+        <form class="add_section" action="#" method="post">
+                <input type="text" name="section_name" placeholder="Section Name">
+                <input type="text" name="section_short_name" placeholder="Section Short Name">
+                <input type="number" name="section_area" placeholder="Section Area">
+                <input type="text" name="status" placeholder="Section Status">
+                <input type="submit" name="section_submit" value="Add a Section">
+        </form>
+        <form class="remove_section" action="#" method="post">
+                <input type="submit" name="section_submit" value="Remove a Section">
+        </form>
+    </body>
+</html>
