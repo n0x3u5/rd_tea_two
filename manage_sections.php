04d1847963214248dc012306a7fd7604184d1c3f
Kakkeswar <joydeepbhattacharjee.88@gmail.com>
2015-06-22 21:42:42 +0530

    minor changes to manage_sections.php

--- manage_sections.php
+++ manage_sections.php
@@ -1,3 +1,12 @@
+<?php
+  if(isset($_POST["section_submit"])) {
+
+  }
+  else {
+
+  }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,11 +14,12 @@
         <title>R.D. Tea | Manage Sections</title>
     </head>
     <body>
-        <form class="add_section" action="#" method="post">
-                <input type="text" name="section_name" placeholder="Section Name">
-                <input type="text" name="section_short_name" placeholder="Section Short Name">
-                <input type="number" name="section_area" placeholder="Section Area">
-                <input type="text" name="status" placeholder="Section Status">
+        <form class="add_section" action="manage_section.php" method="post">
+                <input type="text" name="section_name" placeholder="Section Name" required>
+                <input type="text" name="section_short_name" placeholder="Section Short Name" required>
+                <input type="number" name="section_area" placeholder="Section Area" required>
+                <input type="text" name="status" placeholder="Section Status" required>
+
                 <input type="submit" name="section_submit" value="Add a Section">
         </form>
         <form class="remove_section" action="#" method="post">
