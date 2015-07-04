96242cc3ace618b2c266e4625fbde4582595c538
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2015-07-04 18:28:25 +0530

    Fixed leaf chit table
    Yet to add proper print support

--- leaf_chit.php
+++ leaf_chit.php
@@ -15,7 +15,6 @@
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.css">
-		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="css/stylesheet.css">
     <link rel="icon" href="images/logo_rdtea.png"/>
     <?php $page_id = 11;?>
@@ -36,9 +35,11 @@
 			th{
 				text-align: center;
 			}
-			.col-head{
-				color:#520808;
+			.main-content .btn {
+				box-shadow: none;
 			}
+			div.DTTT { margin-bottom: 0.5em; float: right; }
+    	div.dataTables_wrapper { clear: both; }
     </style>
   </head>
   <body>
@@ -69,36 +70,36 @@
       </div>
 			<div class="main-content">
 
-				<table id="leaf_chit_table" class=" table display" cellspacing="0" width="100%" border="1">
+				<table id="leaf_chit_table" class="table table-hover table-bordered" cellspacing="0" width="100%">
 					<thead>
 						<tr class="col-head">
-								<th rowspan="2">Labour Category</th>
+								<th rowspan="2">Labour<br/>Category</th>
 								<th colspan="9">Unpruned</th>
 								<th colspan="9">Pruned</th>
 								<th colspan="3">Grand Total</th>
 						</tr>
 						<tr>
 							<th>Date<br/>Last<br/>Plucked</th>
-							<th>Round<br/>Days</th>
-							<th>Section</th>
-							<th>Area<br/>Plucked<br/>(Hec.)</th>
-							<th>Pluckers</th>
-							<th>Pluckers<br/>per<br/>Hec.</th>
-							<th>Leaf<br/>Plucked</th>
-							<th>Average<br/>Productivity</th>
+							<th>Rnd.<br/>Days</th>
+							<th>Sec.</th>
+							<th>Area<br/>Plkd<br/>(Hec.)</th>
+							<th>Plkrs.</th>
+							<th>Plkrs<br/>per<br/>Hec.</th>
+							<th>Leaf<br/>Plkd</th>
+							<th>Avg.<br/>Prod.</th>
 							<th>Task</th>
-							<th>Date<br/>Last<br/>Plucked</th>
-							<th>Round<br/>Days</th>
-							<th>Section</th>
-							<th>Area<br/>Plucked<br/>(Hec.)</th>
-							<th>Pluckers</th>
-							<th>Pluckers<br/>per<br/>Hec.</th>
-							<th>Leaf<br/>Plucked</th>
-							<th>Average<br/>Productivity</th>
+							<th>Date<br/>Last<br/>Plkd.</th>
+							<th>Rnd.<br/>Days</th>
+							<th>Sec.</th>
+							<th>Area<br/>Plkd.<br/>(Hec.)</th>
+							<th>Plkrs.</th>
+							<th>Plkrs.<br/>per<br/>Hec.</th>
+							<th>Leaf<br/>Plkd</th>
+							<th>Avg.<br/>Prod.</th>
 							<th>Task</th>
-							<th>Total<br/>Area<br/>Plucked (Hec.)</th>
-							<th>Total<br/>Pluckers</th>
-							<th>Total<br/>Leaf<br/>Plucked</th>
+							<th>Tot.<br/>Area<br/>Plkd.<br/>(Hec.)</th>
+							<th>Tot.<br/>Pluckers</th>
+							<th>Tot.<br/>Leaf<br/>Plkd.</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -127,8 +128,8 @@
 							<td>4660</td>
 						</tr>
 						<tr>
-							<td>Perm. Men</td>
-							<td>27-06-2015</td>
+							<td>Perm. Women</td>
+							<td>28-06-2015</td>
 							<td>1EXTA </td>
 							<td>5</td>
 							<td>112</td>
@@ -137,7 +138,103 @@
 							<td>21</td>
 							<td>7</td>
 							<td>24</td>
-							<td>27-06-2015</td>
+							<td>28-06-2015</td>
+							<td>1EXTA</td>
+							<td>5</td>
+							<td>112</td>
+							<td>22</td>
+							<td>2330</td>
+							<td>21</td>
+							<td>7</td>
+							<td>24</td>
+							<td>10</td>
+							<td>224</td>
+							<td>4660</td>
+						</tr>
+						<tr>
+							<td>Temp. Men</td>
+							<td>29-06-2015</td>
+							<td>1EXTA </td>
+							<td>5</td>
+							<td>112</td>
+							<td>22</td>
+							<td>2330</td>
+							<td>21</td>
+							<td>7</td>
+							<td>24</td>
+							<td>29-06-2015</td>
+							<td>1EXTA</td>
+							<td>5</td>
+							<td>112</td>
+							<td>22</td>
+							<td>2330</td>
+							<td>21</td>
+							<td>7</td>
+							<td>24</td>
+							<td>10</td>
+							<td>224</td>
+							<td>4660</td>
+						</tr>
+						<tr>
+							<td>Temp. Women</td>
+							<td>30-06-2015</td>
+							<td>1EXTA </td>
+							<td>5</td>
+							<td>112</td>
+							<td>22</td>
+							<td>2330</td>
+							<td>21</td>
+							<td>7</td>
+							<td>24</td>
+							<td>30-06-2015</td>
+							<td>1EXTA</td>
+							<td>5</td>
+							<td>112</td>
+							<td>22</td>
+							<td>2330</td>
+							<td>21</td>
+							<td>7</td>
+							<td>24</td>
+							<td>10</td>
+							<td>224</td>
+							<td>4660</td>
+						</tr>
+						<tr>
+							<td>Incentives</td>
+							<td>01-07-2015</td>
+							<td>1EXTA </td>
+							<td>5</td>
+							<td>112</td>
+							<td>22</td>
+							<td>2330</td>
+							<td>21</td>
+							<td>7</td>
+							<td>24</td>
+							<td>01-07-2015</td>
+							<td>1EXTA</td>
+							<td>5</td>
+							<td>112</td>
+							<td>22</td>
+							<td>2330</td>
+							<td>21</td>
+							<td>7</td>
+							<td>24</td>
+							<td>10</td>
+							<td>224</td>
+							<td>4660</td>
+						</tr>
+						<tr>
+							<td>Cash Pluckers</td>
+							<td>02-07-2015</td>
+							<td>1EXTA </td>
+							<td>5</td>
+							<td>112</td>
+							<td>22</td>
+							<td>2330</td>
+							<td>21</td>
+							<td>7</td>
+							<td>24</td>
+							<td>02-07-2015</td>
 							<td>1EXTA</td>
 							<td>5</td>
 							<td>112</td>
@@ -152,7 +249,7 @@
 						</tr>
 						<tr>
 							<td>Perm. Men</td>
-							<td>27-06-2015</td>
+							<td>03-07-2015</td>
 							<td>1EXTA </td>
 							<td>5</td>
 							<td>112</td>
@@ -161,7 +258,7 @@
 							<td>21</td>
 							<td>7</td>
 							<td>24</td>
-							<td>27-06-2015</td>
+							<td>03-07-2015</td>
 							<td>1EXTA</td>
 							<td>5</td>
 							<td>112</td>
@@ -174,7 +271,6 @@
 							<td>224</td>
 							<td>4660</td>
 						</tr>
-
 					</tbody>
 				</table>
 			</div>
@@ -183,24 +279,70 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script src="http://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
-		<script src="//cdn.datatables.net/fixedcolumns/3.0.4/js/dataTables.fixedColumns.min.js"></script>
+		<script src="https://cdn.datatables.net/tabletools/2.2.4/js/dataTables.tableTools.min.js"></script>
 		<script src="https://cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.js"></script>
 		<script>
 			$(function() {
 				$("#datepicker").datepicker({
 					dateFormat: "dd-mm-yy"
 				});
-
-					var table = $('#leaf_chit_table').DataTable( {
-						scrollX: true,
-	        "scrollY": "300px",
-	        "scrollX": "100%",
-	        "scrollCollapse": true,
-	        "paging": false
-	    	} );
-	    	new $.fn.dataTable.FixedColumns( table );
-
+				var table = $('#leaf_chit_table').DataTable({
+						"scrollX": true,
+						"ordering": false,
+						dom: 'T<"clear">lfrtip',
+						tableTools: {
+	            "aButtons": [
+	                "copy",
+	                "xls",
+	                {
+	                    "sExtends": "pdf",
+	                    "sPdfOrientation": "landscape",
+	                },
+	                "print"
+	            	]
+        		},
+						"aoColumns": [
+								null,
+								{ "sType": "date-uk" },
+								null,
+								null,
+								null,
+								null,
+								null,
+								null,
+								null,
+								null,
+								{ "sType": "date-uk" },
+								null,
+								null,
+								null,
+								null,
+								null,
+								null,
+								null,
+								null,
+								null,
+								null,
+								null
+						]
+	    	});
+			// 	var tt = new $.fn.dataTable.TableTools(table);
+    	// 	$( tt.fnContainer() ).insertBefore('div.dataTables_wrapper');
 			});
+			jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+			"date-uk-pre": function ( a ) {
+			var ukDatea = a.split('-');
+			return (ukDatea[2] + ukDatea[1] + ukDatea[0]) * 1;
+			},
+
+			"date-uk-asc": function ( a, b ) {
+			return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+			},
+
+			"date-uk-desc": function ( a, b ) {
+			return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+			}
+		} );
 		</script>
   </body>
 </html>
