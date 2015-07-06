05e3ffbc198128b03ebef7aa4bbc232e243a1d5d
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2015-07-06 19:52:55 +0530

    Fixed the table collapsing in hidden tabs

--- sectional_yearly_review.php
+++ sectional_yearly_review.php
@@ -13,8 +13,8 @@
         <title>R.D. Tea | Yearly Sectional Review</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-        <link rel="stylesheet" href="http://cdn.datatables.net/1.10.7/css/jquery.dataTables.css">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+				<link rel="stylesheet" href="https://cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.css">
 				<link rel="stylesheet" href="css/stylesheet.css">
 				<style rel="stylesheet">
 					.card_style{
@@ -39,37 +39,36 @@
             </div>
 
             <div class="row">
-                <div class="col-sm-2 card_style"><h4>Section Name</h4>
-				<p> 1west</p></div>
+                <div class="col-sm-2 card_style"><h4>Section Name</h4><p> 1west</p></div>
                 <div class="col-sm-2 card_style"><h4>Jat/Clone</h4><p> Genus</p></div>
                 <div class="col-sm-2 card_style"><h4>Shade(temp) Species</h4><p>bioregions</p></div>
                 <div class="col-sm-2 card_style"><h4>Shade (perm.) Species</h4><p>Acer saccharum</p></div>
             </div>
             <div class="row">
-				<div class="col-sm-2 card_style"><h4>Frame(in Inch) HEight</h4><p>10"</p></div>
-				<div class="col-sm-2 card_style"><h4>Bush(in Inch)<br>Height</h4><p> 7"</p></div>
-				<div class="col-sm-2 card_style"><h4>Area</h4>(in Hectare)<p> 120</p></div>
-				<div class="col-sm-2 card_style"><h4>Year of Planting</h4><p> 1972</p></div>
-			</div>
-			<div class="row">
-				<div class="col-sm-2 card_style"><h4>Plant spacing</h4><p> 3"</p></div>
-                <div class="col-sm-2 card_style"><h4>TEmporary shade spacing</h4><p> 2"</p></div>
-				<div class="col-sm-2 card_style"><h4>Permanent Shade Spacing</h4><p> 3"</p></div>
-				<div class="col-sm-2 card_style"><h4>Ext./<br>Replant</h4><p> Y</p></div>
-			</div>
-			<div class="row">
-				<div class="col-sm-2 card_style"><h4>Plant Density</h4><p> Poor</p></div>
-				<div class="col-sm-2 card_style"><h4>Drain-status</h4><p> medium</p></div>
-				<div class="col-sm-2 card_style"><h4>Bush<br>popolation</h4><p> Poor</p></div>
-				<div class="col-sm-2 card_style"><h4>Soil type and<br>topography</h4><p>rich</p></div>
-			</div>
+							<div class="col-sm-2 card_style"><h4>Frame(in Inch) HEight</h4><p>10"</p></div>
+							<div class="col-sm-2 card_style"><h4>Bush(in Inch)<br>Height</h4><p> 7"</p></div>
+							<div class="col-sm-2 card_style"><h4>Area</h4>(in Hectare)<p> 120</p></div>
+							<div class="col-sm-2 card_style"><h4>Year of Planting</h4><p> 1972</p></div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2 card_style"><h4>Plant spacing</h4><p> 3"</p></div>
+			        <div class="col-sm-2 card_style"><h4>TEmporary shade spacing</h4><p> 2"</p></div>
+							<div class="col-sm-2 card_style"><h4>Permanent Shade Spacing</h4><p> 3"</p></div>
+							<div class="col-sm-2 card_style"><h4>Ext./<br>Replant</h4><p> Y</p></div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2 card_style"><h4>Plant Density</h4><p> Poor</p></div>
+							<div class="col-sm-2 card_style"><h4>Drain-status</h4><p> medium</p></div>
+							<div class="col-sm-2 card_style"><h4>Bush<br>popolation</h4><p> Poor</p></div>
+							<div class="col-sm-2 card_style"><h4>Soil type and<br>topography</h4><p>rich</p></div>
+						</div>
 
             <div class="tab-container" style="margin-top:10px;">
                 <ul class="nav nav-tabs nav-justified">
                     <li class="active"><a href="#tab1" data-toggle="tab">Prune </a></li>
                     <li><a href="#tab2" data-toggle="tab">Soil  </a></li>
                 </ul>
-                <div class="tab-content" >
+                <div class="tab-content">
                     <div class="tab-pane active" id="tab1">
                             <table  id="prune_year" class="display table table-hover" border="1" cellspacing="0" width="100%">
                                 <thead  style="background-color:#A5D6A7">
@@ -92,7 +91,7 @@
                                 <tbody>
                                     <tr>
 
-                                        <td   style="text-align:center">19522220</td>
+                                        <td   style="text-align:center">27-06-2015</td>
                                         <td   style="text-align:center">2</td>
                                         <td   style="text-align:center">3</td>
                                         <td   style="text-align:center">4</td>
@@ -104,7 +103,55 @@
                                     </tr>
                                     <tr>
 
-                                        <td   style="text-align:center">1245545151</td>
+                                        <td   style="text-align:center">28-06-2015</td>
+                                        <td   style="text-align:center">23</td>
+                                        <td   style="text-align:center">34</td>
+                                        <td   style="text-align:center">45</td>
+                                        <td   style="text-align:center">56</td>
+                                        <td   style="text-align:center">67</td>
+                                        <td   style="text-align:center">78</td>
+                                        <td   style="text-align:center">89</td>
+                                        <td   style="text-align:center">The draining system is not performing properly.</td>
+                                    </tr>
+																		<tr>
+
+                                        <td   style="text-align:center">29-06-2015</td>
+                                        <td   style="text-align:center">23</td>
+                                        <td   style="text-align:center">34</td>
+                                        <td   style="text-align:center">45</td>
+                                        <td   style="text-align:center">56</td>
+                                        <td   style="text-align:center">67</td>
+                                        <td   style="text-align:center">78</td>
+                                        <td   style="text-align:center">89</td>
+                                        <td   style="text-align:center">The draining system is not performing properly.</td>
+                                    </tr>
+																		<tr>
+
+                                        <td   style="text-align:center">30-06-2015</td>
+                                        <td   style="text-align:center">23</td>
+                                        <td   style="text-align:center">34</td>
+                                        <td   style="text-align:center">45</td>
+                                        <td   style="text-align:center">56</td>
+                                        <td   style="text-align:center">67</td>
+                                        <td   style="text-align:center">78</td>
+                                        <td   style="text-align:center">89</td>
+                                        <td   style="text-align:center">The draining system is not performing properly.</td>
+                                    </tr>
+																		<tr>
+
+                                        <td   style="text-align:center">01-07-2015</td>
+                                        <td   style="text-align:center">23</td>
+                                        <td   style="text-align:center">34</td>
+                                        <td   style="text-align:center">45</td>
+                                        <td   style="text-align:center">56</td>
+                                        <td   style="text-align:center">67</td>
+                                        <td   style="text-align:center">78</td>
+                                        <td   style="text-align:center">89</td>
+                                        <td   style="text-align:center">The draining system is not performing properly.</td>
+                                    </tr>
+																		<tr>
+
+                                        <td   style="text-align:center">02-07-2015</td>
                                         <td   style="text-align:center">23</td>
                                         <td   style="text-align:center">34</td>
                                         <td   style="text-align:center">45</td>
@@ -169,12 +216,18 @@
         <script src="http://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
         <script src="http://cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.js">
         </script>
-		<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-		<script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
         <script>
             $(document).ready(function() {
-                    $('#prune_year').dataTable({"scrollX": true});
-                    $('#soil_year').dataTable({"scrollX": true});
+							$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+						    e.target // activated tab
+						    e.relatedTarget // previous tab
+						    var table = $.fn.dataTable.fnTables(true);
+						    if (table.length > 0) {
+					        $(table).dataTable().fnAdjustColumnSizing();
+						    }
+							});
+              // $('#prune_year').dataTable({"scrollX": true});
+              // $('#soil_year').dataTable({"scrollX": true});
             });
         </script>
     </body>
